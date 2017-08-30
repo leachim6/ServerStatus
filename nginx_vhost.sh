8e4587814f8024bbd6a7aafd2f7773e467455c17
Mike Donaghy <mike@mikedonaghy.org>
2017-08-30 04:45:48 -0500

    Updated grep string to filter binary files in vhosts

--- nginx_vhost.sh
+++ nginx_vhost.sh
@@ -3,5 +3,5 @@
 source $(dirname "0")/bash_colors.sh
 
 if pgrep -x "nginx" > /dev/null; then
- grep -m1 -ir server_name /etc/nginx/* |grep -v "#"| sed -r 's/(.*server_name\s*|;)//g' | tr " " "\n" | grep -v "_" | pr -2 -t | while read line; do /bin/echo -e "$Pur|$BGre$line"; done
+ grep -I -m1 -ir server_name /etc/nginx/* |grep -v "#"| sed -r 's/(.*server_name\s*|;)//g' | tr " " "\n" | grep -v "_" | pr -2 -t | while read line; do /bin/echo -e "$Pur|$BGre$line"; done
 fi
